c53b7394262e8eb3f2e105131abb6b6cdbdebd86
coucoumonecho <coucoumonecho@gmail.com>
2025-04-01 16:42:46 +0800

    simple server board cast

--- main/server.go
+++ main/server.go
@@ -3,35 +3,81 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// online map
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// msg chan
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	return &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 }
 
-func (this *Server) handler(conn net.Conn) {
-	// ...
-	fmt.Println("connect success!")
+func (this *Server) listenMessage() {
+	for {
+		msg := <-this.Message
 
+		this.mapLock.Lock()
+
+		// send all
+		for _, client := range this.OnlineMap {
+			client.C <- msg
+		}
+
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) broadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+func (this *Server) handler(conn net.Conn) {
+	//fmt.Println("connect success!")
+
+	// lock
+	this.mapLock.Lock()
+
+	// online map
+	user := NewUser(conn)
+	this.OnlineMap[user.Name] = user
+
+	// unlock
+	this.mapLock.Unlock()
+
+	// broad cast
+	this.broadCast(user, "is online!")
+
+	// clog
+	select {}
 }
 
 func (this *Server) Start() {
+
 	// listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
-	// close listen socket
-	defer listener.Close()
+
+	// listen
+	go this.listenMessage()
 
 	for {
 		// accept
@@ -44,4 +90,7 @@ func (this *Server) Start() {
 		// do handler
 		go this.handler(conn)
 	}
+
+	// close listen socket
+	listener.Close()
 }
---
+++ main/user.go
@@ -0,0 +1,35 @@
+package main
+
+import "C"
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	addr := conn.RemoteAddr().String()
+
+	user := &User{
+		Name: addr,
+		Addr: addr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	// listen
+	go user.onMessage()
+
+	return user
+}
+
+func (this *User) onMessage() {
+	for {
+		msg := <-this.C
+
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
